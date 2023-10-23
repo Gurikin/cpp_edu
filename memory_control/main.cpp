2450dafdc0d33232d1d6c2a3a0f8bdd270b39678
harry_work <gurikin@yandex.ru>
2023-10-23 15:37:19 +0300

    memory_control add time info to threads

--- memory_control/main.cpp
+++ memory_control/main.cpp
@@ -1,16 +1,16 @@
 #include "Vector.h"
 #include <algorithm>
 #include <iostream>
+#include <sys/time.h>
 #include <thread>
 #include <vector>
-#include <sys/time.h>
 
 using namespace std;
 
 void task(string msg) {
   timeval tm;
   gettimeofday(&tm, NULL);
-  cout << msg << ":" << tm.tv_sec << tm.tv_usec / 1000.0 + 0.5 << endl;
+  cout << msg << " at:\t" << tm.tv_sec << "." << tm.tv_usec / 1000 << "\t(sec)" << endl;
   this_thread::sleep_for(std::chrono::milliseconds(500));
 }
 
@@ -21,7 +21,7 @@ Vector init(int n) {
     t.join();
     myThreads.push_back(std::move(t)); // перемещаем t в пул потоков
   }
-  
+
   Vector vec(n);
   for (int i = 0; i < vec.size(); i++) {
     vec[i] = 777;
@@ -30,8 +30,8 @@ Vector init(int n) {
 }
 
 int main(int, char **) {
-    auto v = init(5); // стартуем потоки из пула и инициализируем v как Vector
-    for (int i = 0; i < v.size(); i++) {
-        cout << "Vector[" << i << "] = " << v[i] << endl;
-    }
+  auto v = init(5); // стартуем потоки из пула и инициализируем v как Vector
+  for (int i = 0; i < v.size(); i++) {
+    cout << "Vector[" << i << "] = " << v[i] << endl;
+  }
 }
